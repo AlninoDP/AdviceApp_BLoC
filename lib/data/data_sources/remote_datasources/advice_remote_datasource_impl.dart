81a58cdb9a56c32f39699aadb412f9d74ca8a962
AlninoDP <alninodp@gmail.com>
2023-08-24 15:25:28 +0700

    implementing Advice API

--- lib/data/data_sources/remote_datasources/advice_remote_datasource_impl.dart
+++ lib/data/data_sources/remote_datasources/advice_remote_datasource_impl.dart
@@ -1,10 +1,24 @@
+import 'dart:convert';
+
 import 'package:adviceapp/data/data_sources/remote_datasources/advice_remote_datasource.dart';
 import 'package:adviceapp/data/models/advice_model.dart';
+import 'package:http/http.dart' as http;
 
 class AdviceRemoteDatasourceImpl implements AdviceRemoteDatasource {
+  final client = http.Client();
   @override
-  Future<AdviceModel> getRandomAdviceFromApi() {
-    // TODO: implement getRandomAdviceFromApi
-    throw UnimplementedError();
+  Future<AdviceModel> getRandomAdviceFromApi() async {
+    /// send http request and put the http response in this variable
+    final response = await client.get(
+        Uri.parse('https://api.flutter-community.com/api/v1/advice'),
+        headers: {'content-type': 'application/json'});
+
+    /// put the response body into this variable and conver it from string to json
+    /// since json type is like a Map, this variable will have like this
+    /// 'advice_id' : 'value',
+    /// 'advice':'value'
+    final responseBody = json.decode(response.body);
+
+    return AdviceModel.fromJson(responseBody);
   }
 }
--- lib/data/repositories/advice_repository_impl.dart
+++ lib/data/repositories/advice_repository_impl.dart
@@ -1,12 +1,15 @@
+import 'package:adviceapp/data/data_sources/remote_datasources/advice_remote_datasource_impl.dart';
 import 'package:adviceapp/domain/entities/advice_entity.dart';
 import 'package:adviceapp/domain/failures/failures.dart';
 import 'package:adviceapp/domain/repositories/advice_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class AdviceRepositoryImpl implements AdviceRepository {
+  final AdviceRemoteDatasourceImpl adviceRemoteDatasourceImpl =
+      AdviceRemoteDatasourceImpl();
   @override
-  Future<Either<AdviceEntity, Failures>> getAdviceFromDatasource() {
-    // TODO: implement getAdviceFromDatasource
-    throw UnimplementedError();
+  Future<Either<AdviceEntity, Failures>> getAdviceFromDatasource() async {
+    final result = await adviceRemoteDatasourceImpl.getRandomAdviceFromApi();
+    return left(result);
   }
 }
